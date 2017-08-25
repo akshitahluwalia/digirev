0ae17863d66d7951129f90df1813ee25af246c12
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-08-26 01:45:39 +0530

    pages added

---
+++ src/UserHistory.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class UserHistory extends Component{
+
+
+	render (){
+		
+		return (
+		<div>
+		<h1>this is UserHistory</h1>
+			</div>
+
+
+		);
+
+	}
+}
+	export default UserHistory
--- src/index.js
+++ src/index.js
@@ -4,19 +4,37 @@ import App from './App'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Navbar.js';
-
+import Comm from './Comm.js';
+import UserHistory from './UserHistory.js';
+import Seekers from './Seekers.js';
+import ApplyForm from './ApplyForm.js';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
  
 injectTapEventPlugin();
+
+
 const Main = () => (
   <MuiThemeProvider>
- <div> 
+  
+ <div>
  	<Navbar/>
- 	<App/>
+ <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/comm" component={Comm} />
+        <Route exact path="/history" component={UserHistory} />
+        <Route exact path="/seekers" component={Seekers} />
+        <Route exact path="/applyform" component={ApplyForm} />
+        
+      </div>
+    </Router> 
+ 	
  </div>
  </MuiThemeProvider>
  );
 
 ReactDOM.render(
+
 	<Main />,
   document.getElementById('root')
 );
